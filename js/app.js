d0f50f976e6bfa3f8136abee48283c8c74b747d4
Feltsnacks <Feltsncaks@users.noreply.github.com>
2014-03-30 19:16:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,3 +8,16 @@
  * @type {angular.Module}
  */
 var todomvc = angular.module('todomvc', []);
+
+function showScreenSize(){
+	
+	var size = 'Screen width: '+window.screen.width
+		+'Screen height: '+window.screen.height
+		+'Screen availWidth: '+window.screen.availWidth
+		+'Screen availHeight: '+window.screen.availHeight
+		+'window.innerWidth: '+window.innerWidth
+		+'window.innerHeight: '+window.innerHeight;
+	
+	alert(size);
+		
+}
